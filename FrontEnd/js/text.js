7109ace8061ec9549862e356ed407f3968855f0f
N-ngochoa <ngochoa.h104@gmail.com>
2019-10-09 16:32:54 +0700

    9/10/2019

--- FrontEnd/js/text.js
+++ FrontEnd/js/text.js
@@ -3,10 +3,12 @@ var modal = document.getElementById("myModal");
 var modall = document.getElementById("myModall");
 var modalll = document.getElementById("myModalll");
 
+
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 var btnn = document.getElementById("myBtnn");
 var btnnn = document.getElementById("myBtnnn");
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -21,6 +23,7 @@ btnn.onclick = function() {
 btnnn.onclick = function() {
   modalll.style.display = "block";
 }
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
